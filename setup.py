9d7cd0a9b8337443775fc7bdb8513abd2b791e95
Daniel Sheppard <dans@dansheps.com>
2021-09-26 09:55:04 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox-secretstore',
-    version='1.0.5',
+    version='1.0.6',
     description='A Secret store for NetBox',
     url='https://github.com/dansheps/netbox-secretstore',
     author='Daniel Sheppard',
