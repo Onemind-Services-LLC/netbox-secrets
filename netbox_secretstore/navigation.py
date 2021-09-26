bdac4f987bee7036267151ca4424ecbeea5d98b4
Daniel Sheppard <dans@dansheps.com>
2021-09-26 09:54:49 -0500

    Fixes #33 - Moves Secrets to plugin menu system

--- netbox_secretstore/navigation.py
+++ netbox_secretstore/navigation.py
@@ -1,59 +1,41 @@
-from netbox import navigation_menu
+from extras.plugins import PluginMenuItem, PluginMenuButton
 
-SECRETS_MENU = navigation_menu.Menu(
-    label="Secrets",
-    icon_class="mdi mdi-key",
-    groups=(
-        navigation_menu.MenuGroup(
-            label="User",
-            items=(
-                navigation_menu.MenuItem(
-                    link_text="User Key", link="plugins:netbox_secretstore:userkey"
-                ),
+menu_items = (
+    PluginMenuItem(link_text="User Key", link="plugins:netbox_secretstore:userkey"),
+    PluginMenuItem(
+        link_text="Secret Roles",
+        link="plugins:netbox_secretstore:secretrole_list",
+        buttons=(
+            PluginMenuButton(
+                link="plugins:netbox_secretstore:secretrole_add",
+                title="Add Secret Role",
+                icon_class="mdi mdi-plus-thick",
+                color="success",
+            ),
+            PluginMenuButton(
+                link="plugins:netbox_secretstore:secretrole_import",
+                title="Import Secret Role",
+                icon_class="mdi mdi-upload",
+                color="info",
             ),
         ),
-        navigation_menu.MenuGroup(
-            label="Secrets",
-            items=(
-                navigation_menu.MenuItem(
-                    link_text="Secret Roles",
-                    link="plugins:netbox_secretstore:secretrole_list",
-                    buttons=(
-                        navigation_menu.MenuItemButton(
-                            link="plugins:netbox_secretstore:secretrole_add",
-                            title="Add Secret Role",
-                            icon_class="mdi mdi-plus-thick",
-                            color="success",
-                        ),
-                        navigation_menu.MenuItemButton(
-                            link="plugins:netbox_secretstore:secretrole_import",
-                            title="Import Secret Role",
-                            icon_class="mdi mdi-upload",
-                            color="info",
-                        ),
-                    ),
-                ),
-                navigation_menu.MenuItem(
-                    link_text="Secrets",
-                    link="plugins:netbox_secretstore:secret_list",
-                    buttons=(
-                        navigation_menu.MenuItemButton(
-                            link="plugins:netbox_secretstore:secret_add",
-                            title="Add Secret",
-                            icon_class="mdi mdi-plus-thick",
-                            color="success",
-                        ),
-                        navigation_menu.MenuItemButton(
-                            link="plugins:netbox_secretstore:secret_import",
-                            title="Import Secret",
-                            icon_class="mdi mdi-upload",
-                            color="info",
-                        ),
-                    ),
-                ),
+    ),
+    PluginMenuItem(
+        link_text="Secrets",
+        link="plugins:netbox_secretstore:secret_list",
+        buttons=(
+            PluginMenuButton(
+                link="plugins:netbox_secretstore:secret_add",
+                title="Add Secret",
+                icon_class="mdi mdi-plus-thick",
+                color="success",
+            ),
+            PluginMenuButton(
+                link="plugins:netbox_secretstore:secret_import",
+                title="Import Secret",
+                icon_class="mdi mdi-upload",
+                color="info",
             ),
         ),
     ),
 )
-
-navigation_menu.MENUS.append(SECRETS_MENU)
