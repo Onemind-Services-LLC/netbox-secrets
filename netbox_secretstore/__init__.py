bbdfbfd48e57df279aedd349d4d890d311297e11
Daniel Sheppard <dans@dansheps.com>
2021-11-19 08:35:34 -0600

    Version Bump to bring to 1.0.9 for PyPi

--- netbox_secretstore/__init__.py
+++ netbox_secretstore/__init__.py
@@ -1,21 +1,24 @@
 from extras.plugins import PluginConfig
 
 
+try:
+    from importlib.metadata import metadata
+except ModuleNotFoundError:
+    from importlib_metadata import metadata
+
+metadata = metadata('netbox_secretstore')
+
+
 class NetBoxSecretStore(PluginConfig):
-    name = 'netbox_secretstore'
-    verbose_name = 'Netbox Secret Store'
-    description = 'A Secret Storage for NetBox'
-    version = '1.0.8'
-    author = 'NetBox Maintainers'
-    author_email = ''
+    name = metadata.get('Name').replace('-', '_')
+    verbose_name = metadata.get('Summary')
+    description = metadata.get('Description')
+    version = metadata.get('Version')
+    author = metadata.get('Author')
+    author_email = metadata.get('Author-email')
     base_url = 'netbox_secretstore'
     min_version = '3.0.0'
     required_settings = []
-    caching_config = {
-        '*': {
-            'ops': 'all'
-        }
-    }
     default_settings = {
         'public_key_size': 2048
     }
