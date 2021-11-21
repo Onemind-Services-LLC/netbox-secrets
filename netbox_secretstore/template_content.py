bcca6f371b7da467b0bf6e9a39e50f7a57c64a98
Daniel Sheppard <dans@dansheps.com>
2021-11-20 23:31:41 -0600

    Closes #48 - Fixes Secret display on virtual machines

--- netbox_secretstore/template_content.py
+++ netbox_secretstore/template_content.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes.models import ContentType
+
 from extras.plugins import PluginTemplateExtension
 from .models import Secret
 
@@ -6,8 +8,15 @@ class Secrets(PluginTemplateExtension):
 
     def right_page(self):
         obj = self.context['object']
+
+        secrets = None
+        if ContentType.objects.get_for_model(obj).name == 'device':
+            secrets = Secret.objects.filter(device=obj)
+        elif ContentType.objects.get_for_model(obj).name == 'virtualmachine':
+            secrets = Secret.objects.filter(virtualmachine=obj)
+
         return self.render('netbox_secretstore/inc/device_secrets.html', extra_context={
-            'secrets': Secret.objects.filter(device=obj),
+            'secrets': secrets,
         })
 
 
@@ -16,7 +25,7 @@ class DeviceSecrets(Secrets):
 
 
 class VMSecrets(Secrets):
-    model = 'virtualization.virtualMachine'
+    model = 'virtualization.virtualmachine'
 
 
 template_extensions = [DeviceSecrets, VMSecrets]
