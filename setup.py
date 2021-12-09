7bcdc9e6d8e2e638e9901c3a9674a7a14ef9ffd2
Daniel Sheppard <dans@dansheps.com>
2021-12-09 09:14:41 -0600

    Version Bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox-secretstore',
-    version='1.0.10',
+    version='1.0.11',
     description='Netbox Secret Store',
     long_description='A Secret store for NetBox',
     url='https://github.com/dansheps/netbox-secretstore/',
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         'importlib',
         'pycryptodome',
-        'netbox-plugin-extensions'
+        'netbox-plugin-extensions>=1.0.8'
     ],
     packages=find_packages(),
     include_package_data=True,
