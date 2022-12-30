9f4e6fd04ffd29d3da62038c10e9b5cabf03bbd5
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2022-12-31 00:56:35 +0530

    updated navigation permissions

--- netbox_secrets/navigation.py
+++ netbox_secrets/navigation.py
@@ -1,40 +1,50 @@
 from extras.plugins import PluginMenuItem, PluginMenuButton
 
 menu_items = (
-    PluginMenuItem(link_text="User Key", link="plugins:netbox_secrets:userkey"),
+    PluginMenuItem(
+        link_text="User Key",
+        link="plugins:netbox_secrets:userkey",
+        permissions=["netbox_secrets.view_userkey"],
+    ),
     PluginMenuItem(
         link_text="Secret Roles",
         link="plugins:netbox_secrets:secretrole_list",
+        permissions=["netbox_secrets.view_secretrole"],
         buttons=(
             PluginMenuButton(
                 link="plugins:netbox_secrets:secretrole_add",
                 title="Add Secret Role",
                 icon_class="mdi mdi-plus-thick",
                 color="green",
+                permissions=["netbox_secrets.add_secretrole"],
             ),
             PluginMenuButton(
                 link="plugins:netbox_secrets:secretrole_import",
                 title="Import Secret Role",
                 icon_class="mdi mdi-upload",
                 color="teal",
+                permissions=["netbox_secrets.add_secretrole"],
             ),
         ),
     ),
     PluginMenuItem(
         link_text="Secrets",
         link="plugins:netbox_secrets:secret_list",
+        permissions=["netbox_secrets.view_secret"],
         buttons=(
             PluginMenuButton(
                 link="plugins:netbox_secrets:secret_add",
                 title="Add Secret",
                 icon_class="mdi mdi-plus-thick",
                 color="green",
+                permissions=["netbox_secrets.add_secret"],
             ),
             PluginMenuButton(
                 link="plugins:netbox_secrets:secret_import",
                 title="Import Secret",
                 icon_class="mdi mdi-upload",
                 color="teal",
+                permissions=["netbox_secrets.add_secret"],
             ),
         ),
     ),
