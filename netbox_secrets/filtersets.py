7cca5030042e271648a0d5d8f603ba537063bbf9
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2023-01-19 20:36:01 +0530

    fixed secrets filterset

--- netbox_secrets/filtersets.py
+++ netbox_secrets/filtersets.py
@@ -40,45 +40,36 @@ class SecretRoleFilterSet(NetBoxModelFilterSet):
         )
 
 
-class SecretFilterSetMixin:
-    q = django_filters.CharFilter(
-        method='search',
-        label='Search',
-    )
-
-    name = django_filters.ModelMultipleChoiceFilter(
-        queryset=Secret.objects.all(),
-        field_name='name',
-        label='Name',
-    )
-
-    role_id = django_filters.ModelMultipleChoiceFilter(
-        queryset=SecretRole.objects.all(),
-        label='Role (ID)',
-    )
-    role = django_filters.ModelMultipleChoiceFilter(
-        field_name='role__slug',
-        queryset=SecretRole.objects.all(),
-        to_field_name='slug',
-        label='Role (slug)',
-    )
-
-    assigned_object_type_id = django_filters.ModelMultipleChoiceFilter(
-        field_name='assigned_object_type',
-        queryset=ContentType.objects.filter(SECRET_ASSIGNABLE_MODELS),
-        label='Object type (ID)',
-    )
-
-    def search(self, queryset, name, value):
-        if not value.strip():
-            return queryset
-        return queryset.filter(
-            Q(name__icontains=value)
-        )
-
-
 if plugin_settings.get('enable_contacts', False):
     class SecretFilterSet(SecretFilterSetMixin, NetBoxModelFilterSet):
+        q = django_filters.CharFilter(
+            method='search',
+            label='Search',
+        )
+
+        name = django_filters.ModelMultipleChoiceFilter(
+            queryset=Secret.objects.all(),
+            field_name='name',
+            label='Name',
+        )
+
+        role_id = django_filters.ModelMultipleChoiceFilter(
+            queryset=SecretRole.objects.all(),
+            label='Role (ID)',
+        )
+        role = django_filters.ModelMultipleChoiceFilter(
+            field_name='role__slug',
+            queryset=SecretRole.objects.all(),
+            to_field_name='slug',
+            label='Role (slug)',
+        )
+
+        assigned_object_type_id = django_filters.ModelMultipleChoiceFilter(
+            field_name='assigned_object_type',
+            queryset=ContentType.objects.filter(SECRET_ASSIGNABLE_MODELS),
+            label='Object type (ID)',
+        )
+
         contact = django_filters.ModelMultipleChoiceFilter(
             field_name='contacts__contact',
             queryset=Contact.objects.all(),
@@ -88,8 +79,50 @@ if plugin_settings.get('enable_contacts', False):
         class Meta:
             model = Secret
             fields = ['id', 'assigned_object_type_id', 'assigned_object_id', 'role_id', 'role', 'name', 'contact']
+
+        def search(self, queryset, name, value):
+            if not value.strip():
+                return queryset
+            return queryset.filter(
+                Q(name__icontains=value)
+            )
 else:
-    class SecretFilterSet(SecretFilterSetMixin, NetBoxModelFilterSet):
+    class SecretFilterSet(NetBoxModelFilterSet):
+        q = django_filters.CharFilter(
+            method='search',
+            label='Search',
+        )
+
+        name = django_filters.ModelMultipleChoiceFilter(
+            queryset=Secret.objects.all(),
+            field_name='name',
+            label='Name',
+        )
+
+        role_id = django_filters.ModelMultipleChoiceFilter(
+            queryset=SecretRole.objects.all(),
+            label='Role (ID)',
+        )
+        role = django_filters.ModelMultipleChoiceFilter(
+            field_name='role__slug',
+            queryset=SecretRole.objects.all(),
+            to_field_name='slug',
+            label='Role (slug)',
+        )
+
+        assigned_object_type_id = django_filters.ModelMultipleChoiceFilter(
+            field_name='assigned_object_type',
+            queryset=ContentType.objects.filter(SECRET_ASSIGNABLE_MODELS),
+            label='Object type (ID)',
+        )
+
         class Meta:
             model = Secret
             fields = ['id', 'assigned_object_type_id', 'assigned_object_id', 'role_id', 'role', 'name']
+
+        def search(self, queryset, name, value):
+            if not value.strip():
+                return queryset
+            return queryset.filter(
+                Q(name__icontains=value)
+            )
