f1e05569d835791b38ee7f6b7117e4cc453bdf4e
kirk444 <scott.silverman@gmail.com>
2023-04-21 10:28:09 -0500

    Update netbox_secrets/api/views.py
    also add MultiPartParser for checks
    
    Co-authored-by: uedvt359 <54625981+uedvt359@users.noreply.github.com>
    Signed-off-by: kirk444 <scott.silverman@gmail.com>

--- netbox_secrets/api/views.py
+++ netbox_secrets/api/views.py
@@ -7,7 +7,7 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from netbox.api.viewsets import NetBoxModelViewSet
 from rest_framework.exceptions import ValidationError
-from rest_framework.parsers import  JSONParser, FormParser
+from rest_framework.parsers import  JSONParser, FormParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.routers import APIRootView
