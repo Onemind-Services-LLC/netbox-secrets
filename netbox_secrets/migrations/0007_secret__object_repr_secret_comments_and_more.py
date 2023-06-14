3a45362a476ab86a564fff0016ab8ae21bdaa888
kprince28 <pkumar@onemindservices.com>
2023-06-14 10:33:45 +0530

    fix migration to skip deleted devices and virtual_machines

--- netbox_secrets/migrations/0007_secret__object_repr_secret_comments_and_more.py
+++ netbox_secrets/migrations/0007_secret__object_repr_secret_comments_and_more.py
@@ -6,15 +6,16 @@ from django.db import migrations, models
 
 def copy_assigned_object(apps, schema_editor):
     Secret = apps.get_model('netbox_secrets', 'Secret')
-    try:
-        for secret in Secret.objects.all():
-            content_type = ContentType.objects.get(id=secret.assigned_object_type_id)
-            Model = apps.get_model(content_type.app_label, str(content_type.model).capitalize())
-            object = Model.objects.filter(id=secret.assigned_object_id).first()
-            secret._object_repr = object.name
-            secret.save()
-    except:
-        pass
+    for secret in Secret.objects.all():
+        content_type = ContentType.objects.get(id=secret.assigned_object_type_id)
+        Model = apps.get_model(content_type.app_label, str(content_type.model).capitalize())
+        object = Model.objects.filter(id=secret.assigned_object_id).first()
+        if not object:
+            continue
+        secret._object_repr = object.name
+        secret.save()
+
+
 
 
 class Migration(migrations.Migration):
