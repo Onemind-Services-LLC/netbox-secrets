a50ba1dea0e677928befc2345854c8f1b546324b
Prince Kumar <pkumar@onemindservices.com>
2024-07-16 14:56:32 +0530

    NB-57 bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(readme) as fh:
 
 setup(
     name='netbox-secrets',
-    version='2.0.0',
+    version='2.0.1',
     description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
