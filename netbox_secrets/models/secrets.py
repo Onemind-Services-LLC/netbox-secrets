838ba3a6d0323d68211ec6bb2c93689ade95962b
Prince Kumar <pkumar@onemindservices.com>
2025-03-20 17:09:23 +0530

    OMS-686 optimized imports

--- netbox_secrets/models/secrets.py
+++ netbox_secrets/models/secrets.py
@@ -1,6 +1,5 @@
 import os
-from django.utils.translation import gettext_lazy as _
-from django.db.models import F, ProtectedError
+
 from Crypto.Cipher import AES
 from Crypto.PublicKey import RSA
 from Crypto.Util import strxor
@@ -10,6 +9,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelatio
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import ProtectedError
 from django.urls import reverse
 from django.utils.encoding import force_bytes
 
