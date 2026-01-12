2f4d7f14950e0351043216509c24f96e254fa34d
Prince Kumar <pkumar@onemindservices.com>
2026-01-12 16:37:56 +0530

    OMS-1137 reorganize models into separate files

--- netbox_secrets/models/__init__.py
+++ netbox_secrets/models/__init__.py
@@ -1,1 +1,23 @@
-from .secrets import *  # noqa
+"""
+NetBox Secrets Plugin Models.
+
+This package contains all models for the NetBox Secrets plugin:
+
+Key Management (keys.py):
+- UserKey: RSA public key storage and master key encryption
+- SessionKey: Temporary session keys for secret encryption/decryption
+
+Secret Storage (secrets.py):
+- SecretRole: Functional classification of secrets
+- Secret: AES-256 encrypted storage for sensitive data
+"""
+
+from .keys import SessionKey, UserKey
+from .secrets import Secret, SecretRole
+
+__all__ = [
+    'UserKey',
+    'SessionKey',
+    'SecretRole',
+    'Secret',
+]
