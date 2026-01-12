51a2e16ee14dccad2a18656727a847ed2040244b
Prince Kumar <pkumar@onemindservices.com>
2026-01-12 16:36:24 +0530

    OMS-1137 fix constants to lookup plugin settings

--- netbox_secrets/constants.py
+++ netbox_secrets/constants.py
@@ -1,16 +1,19 @@
 from django.conf import settings
 from django.db.models import Q
 
-plugin_settings = settings.PLUGINS_CONFIG.get('netbox_secrets')
+plugin_settings = settings.PLUGINS_CONFIG.get("netbox_secrets", {})
 
 #
 # Secrets
 #
 
 SECRET_ASSIGNABLE_MODELS = Q()
-for app_model in plugin_settings.get('apps'):
-    app_label, model = app_model.split('.')
+
+apps = plugin_settings.get("apps", [])
+for app_model in apps:
+    app_label, model = app_model.split(".")
     SECRET_ASSIGNABLE_MODELS |= Q(app_label=app_label, model=model)
+
 
 SECRET_PLAINTEXT_MAX_LENGTH = 65535
 
