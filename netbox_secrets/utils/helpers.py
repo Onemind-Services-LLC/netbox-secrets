7b63f309677073a003c5b800df3fc4f3d8e9a2ff
Prince Kumar <pkumar@onemindservices.com>
2026-01-29 14:06:17 +0530

    OMS-1137 move project static dir to a seperate js file

--- netbox_secrets/utils/helpers.py
+++ netbox_secrets/utils/helpers.py
@@ -9,7 +9,16 @@ def get_session_key(request):
     """
     Extract and decode the session key sent with a request. Returns None if no session key was provided.
     """
-    session_key = request.COOKIES.get(SESSION_COOKIE_NAME, None)
-    if session_key is not None:
+    session_key = request.COOKIES.get(SESSION_COOKIE_NAME)
+    if not session_key:
+        session_key = request.META.get('HTTP_X_SESSION_KEY')
+    if not session_key and hasattr(request, 'POST'):
+        session_key = request.POST.get('session_key')
+
+    if session_key is None:
+        return None
+
+    try:
         return base64.b64decode(session_key)
-    return session_key
+    except Exception:
+        return None
