edd1dee01c4d410e0e761420aa09f3143f9a949d
Prince Kumar <pkumar@onemindservices.com>
2026-03-11 15:41:24 +0530

    Fixes: #238 Avoid database queries during plugin initialization

--- netbox_secrets/signals.py
+++ netbox_secrets/signals.py
@@ -1,7 +1,8 @@
 import sys
 
+from django.apps import apps
+from django.conf import settings
 from django.contrib.contenttypes.fields import GenericRelation
-from django.contrib.contenttypes.models import ContentType
 from django.db import ProgrammingError
 from django.db.backends.postgresql.base import DatabaseWrapper
 from django.db.backends.signals import connection_created
@@ -14,23 +15,28 @@ def configure_generic_relations(sender, **kwargs):
         # Skip this signal during tests to avoid issues with test database destruction
         return
 
-    from .constants import SECRET_ASSIGNABLE_MODELS
+    plugin_settings = settings.PLUGINS_CONFIG.get("netbox_secrets", {})
+    model_list = plugin_settings.get("apps", [])
 
     try:
-        for content_type in ContentType.objects.filter(SECRET_ASSIGNABLE_MODELS):
-            model_class = content_type.model_class()
+        for model_path in model_list:
+            try:
+                app_label, model_name = model_path.split(".", 1)
+            except ValueError:
+                continue
+
+            model_class = apps.get_model(app_label, model_name)
+
             if model_class is None:
                 continue
-            if not hasattr(model_class, 'secrets'):
+
+            if not hasattr(model_class, "secrets"):
                 GenericRelation(
-                    to='netbox_secrets.Secret',
-                    content_type_field='assigned_object_type',
-                    object_id_field='assigned_object_id',
-                    related_query_name=str(content_type.model),
-                ).contribute_to_class(
-                    model_class,
-                    'secrets',
-                )
+                    to="netbox_secrets.Secret",
+                    content_type_field="assigned_object_type",
+                    object_id_field="assigned_object_id",
+                    related_query_name=model_name,
+                ).contribute_to_class(model_class, "secrets")
 
     except ProgrammingError:
         # DB may still not be ready during very early migrations
